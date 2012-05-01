d910fc4335eec247f27463b67e4f7ac24b11d765
mattc <matthew3cord@gmail.com>
2012-05-01 11:44:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoneyDo::Application.config.secret_token = '924140cd226b951bd8be0bfc97d93143ef1d54875e4c81de3d83ae53e55176fa6a37806746bef7a8fa94cbfea356e93900d278e89ac6b30bd5923439709eafb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HoneyDo::Application.config.session_store :cookie_store, key: '_Honey-Do_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HoneyDo::Application.config.session_store :active_record_store
