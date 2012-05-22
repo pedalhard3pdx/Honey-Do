134a953ea64d51ee48c01ef97052201e39de3a43
Matthew Cordeiro <matthew3cord@gmail.com>
2012-05-22 16:00:23 -0700

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 HoneyDo::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/contact"
+  root :to => 'pages#home'
 
-  get "pages/about"
+  
+
+end
+
+  
 
   
   # The priority is based upon order of creation:
