66a8af940c5bf04c21a76a693032ca9673fbfa98
Matthew Cordeiro <matthew3cord@gmail.com>
2012-05-22 21:51:17 -0700

    Added logo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
 
-# Return a title on a per-page basis.
-    def title
-		base_title = "Honey-Do-List"
-		if @title.nil?
-			base_title
-		else
-			"#{base_title} |  #{@title}"
-		end
+  # Return a title on a per-page basis.
+  def title
+	base_title = "Honey-Do-List"
+	if @title.nil?
+	  base_title
+	else
+	  "#{base_title} |  #{@title}"
 	end
+  end
+  def logo
+  	image_tag("logo.png", :alt => "Honey-Do-List", :class => "round")
+  end
 end
