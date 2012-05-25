d64480955e659dfb8be8ec3f0014da80fce2b704
Matthew Cordeiro <matthew3cord@gmail.com>
2012-05-24 20:16:47 -0700

    Added a stub User show page

--- db/migrate/20120524201212_add_email_uniqueness_index.rb
+++ db/migrate/20120524201212_add_email_uniqueness_index.rb
@@ -1,7 +1,11 @@
 class AddEmailUniquenessIndex < ActiveRecord::Migration
-  def up
+  def up 
+  	add_index :users, :email, :unique => true
+
   end
 
   def down
+  	remove_index :users, :email
+  	
   end
 end
