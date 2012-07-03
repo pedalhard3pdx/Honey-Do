8f9f95bbf12d454058eb406dd2f6fdddf96a6180
Matthew Cordeiro <matthew3cord@gmail.com>
2012-07-02 19:28:05 -0700

    Quick style edit

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -55,7 +55,7 @@ describe "Users" do
         user = Factory(:user)
       	visit signin_path
       	fill_in "Email",    :with => user.email
-      	fill_in "Password",    :with => user.password
+      	fill_in "Password", :with => user.password
       	click_button
       	controller.should be_signed_in
       	click_link "Sign out"
